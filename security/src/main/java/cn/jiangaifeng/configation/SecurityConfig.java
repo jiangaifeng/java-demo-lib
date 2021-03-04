900ea3fffc57fc5ed8ef4add70cef438ff343b77
jiangaifeng <jiangaifeng@inspur.com>
2021-03-04 17:30:34 +0800

    修改security

--- security/src/main/java/cn/jiangaifeng/configation/SecurityConfig.java
+++ security/src/main/java/cn/jiangaifeng/configation/SecurityConfig.java
@@ -1,7 +1,5 @@
 package cn.jiangaifeng.configation;
 
-import cn.jiangaifeng.handler.MyAuthentiationFailHandler;
-import cn.jiangaifeng.handler.MyAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 
@@ -18,10 +17,10 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private MyAuthenticationSuccessHandler myAuthenticationSuccessHandler;
+    private AuthenticationSuccessHandler myAuthenticationSuccessHandler;
 
     @Autowired
-    private MyAuthentiationFailHandler myAuthentiationFailHandler;
+    private AuthenticationFailureHandler myAuthentiationFailHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -30,15 +29,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin() // 表单登录。
-                // http.httpBasic() //basic登录
-                .loginPage("/authentication/require")
+        http.formLogin() // 表单登录
+                .loginPage("/authentication/require")//验证失败重定向地址
                 .loginProcessingUrl("/api/auth/login")
                 .successHandler( myAuthenticationSuccessHandler )
                 .failureHandler( myAuthentiationFailHandler )
                 .and()
                 .authorizeRequests() // 对请求授权
-                .antMatchers("authentication/require").permitAll()
+                .antMatchers("/authentication/require").permitAll()
                 .anyRequest() // 任何请求
                 .authenticated()// 需要身份认证
                 .and()
--- security/src/main/java/cn/jiangaifeng/controller/SecurityController.java
+++ security/src/main/java/cn/jiangaifeng/controller/SecurityController.java
@@ -4,20 +4,20 @@ import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@RestController
+@Controller
 public class SecurityController {
 
-    private RequestCache requestCache = new HttpSessionRequestCache();
-    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-
     //当需要身份认证时，跳转到这里
-    @RequestMapping("authentication/require")
+    @RequestMapping("/authentication/require")
+    @ResponseBody
     public String requireAuthentication(HttpServletRequest request, HttpServletResponse response ){
         return "访问的服务需要认证";
     }
